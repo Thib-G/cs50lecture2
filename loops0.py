bb86e06a19309288f8316028d669672535ccb3c4
Thib <thibaut.g@protonmail.com>
2018-07-26 12:53:04 +0200

    Try to follow video

--- loops0.py
+++ loops0.py
@@ -1,2 +1,14 @@
 for i in range(5):
     print(i)
+
+names = ["Alice", "Bob", "Charlie"]
+
+for name in names:
+    print(name)
+
+def squared(x):
+    return x ** 2
+
+for i in range(10):
+    print("{} squared equals {}".format(i, squared(i)))
+
